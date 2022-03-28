9de640d1a65bd0e9ee0a11e9d8f7849b75ebc982
TeerapongP <std25406@snws.ac.th>
2022-03-28 22:35:01 +0700

    add

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/index")
+def index():
   return render_template('index.html')
-  
+
+@app.route("/manga_best_seller")
+def manga_best_seller():
+  return render_template('manga_best_seller.html')
+
 @app.route("/signin")
 def signin():
   return render_template('signin.html')
